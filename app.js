bafd16117758abceb42b52768880927a40284cba
navodanilakshi <nilakshinavoda97@gmail.com>
2021-07-30 17:01:07 +0530

    refactoring code

--- app.js
+++ app.js
@@ -1,57 +1,53 @@
+const p1 = {
+	score: 0,
+	button: document.querySelector('#playerOne'),
+	span: document.querySelector('#one')
+};
+const p2 = {
+	score: 0,
+	button: document.querySelector('#playerTwo'),
+	span: document.querySelector('#two')
+};
+const updateScore = (player, opponent) => {
+	if (!isGameOver) {
+		player.score += 1;
+		if (player.score === preference) {
+			isGameOver = true;
+			player.span.classList.add('has-text-danger');
+			opponent.span.classList.add('has-text-success');
+			player.button.disabled = true;
+			opponent.button.disabled = true;
+			reset.innerText = 'Play Again!';
+			select.selectedIndex = 0;
+		}
+		player.span.textContent = player.score;
+	}
+};
+
 const select = document.querySelector('#playingTo');
-const playerOne = document.querySelector('#playerOne');
-const playerTwo = document.querySelector('#playerTwo');
-const spanOne = document.querySelector('#one');
-const spanTwo = document.querySelector('#two');
 const reset = document.querySelector('#reset');
 let preference = 0;
-let playerOneScore = 0;
-let playerTwoScore = 0;
 let isGameOver = false;
 
 select.addEventListener('change', (e) => {
 	preference = parseInt(select.value);
 });
 
-playerOne.addEventListener('click', (e) => {
-	if (!isGameOver) {
-		playerOneScore += 1;
-		if (playerOneScore === preference) {
-			isGameOver = true;
-			spanOne.classList.add('has-text-danger');
-			spanTwo.classList.add('has-text-success');
-			playerOne.disabled = true;
-			playerTwo.disabled = true;
-			reset.innerText = 'Play Again!';
-			select.selectedIndex = 0;
-		}
-		spanOne.textContent = playerOneScore;
-	}
+p1.button.addEventListener('click', (e) => {
+	updateScore(p1, p2);
 });
-playerTwo.addEventListener('click', (e) => {
-	if (!isGameOver) {
-		playerTwoScore += 1;
-		if (playerTwoScore === preference) {
-			isGameOver = true;
-			spanTwo.classList.add('has-text-danger');
-			spanOne.classList.add('has-text-success');
-			playerOne.disabled = true;
-			playerTwo.disabled = true;
-			reset.innerText = 'Play Again!';
-			select.selectedIndex = 0;
-		}
-		spanTwo.textContent = playerTwoScore;
-	}
+p2.button.addEventListener('click', (e) => {
+	updateScore(p2, p1);
 });
 reset.addEventListener('click', (e) => {
-	spanOne.textContent = 0;
-	spanTwo.textContent = 0;
-	spanOne.classList.remove('has-text-danger', 'has-text-success');
-	spanTwo.classList.remove('has-text-danger', 'has-text-success');
-	playerOneScore = 0;
-	playerTwoScore = 0;
+	p1.span.textContent = 0;
+	p2.span.textContent = 0;
+	p1.span.classList.remove('has-text-danger', 'has-text-success');
+	p2.span.classList.remove('has-text-danger', 'has-text-success');
+	p1.score = 0;
+	p2.score = 0;
 	isGameOver = false;
-	playerOne.disabled = false;
-	playerTwo.disabled = false;
+	p1.button.disabled = false;
+	p2.button.disabled = false;
 	reset.innerText = 'Reset';
 });
